f20f18159a0b90571b24c7b5599eb75f043273b9
Alexander Esgen <alexander.esgen@tweag.io>
2025-03-27 13:28:21 +0100

    Honest vote splitting: settlement reference, log scale, more values
    This is more appropriate for the task at hand, and we already use it for the
    expected values in the same plot.
    
    Also derive formula for one part of the calculation analytically.

--- scripts/honest-vote-splitting.py
+++ scripts/honest-vote-splitting.py
@@ -14,38 +14,39 @@ def Psuccess(alpha, perasBlockMinSlots):
     # P(H ≤ A)
     HleA = sum([A.pmf(x) * H.cdf(x) for x in range(perasBlockMinSlots + 1)])
 
-    Hp = stats.geom(phi(1 - alpha))
-    # P(Ap ≥ 1) where Ap ~ Binom(Hp, phi(alpha))
-    cutoff = int(Hp.ppf(1 - 1e-10))
-    Apge1 = sum([Hp.pmf(x + 1) * stats.binom(x, phi(alpha)).sf(0) for x in range(cutoff)])
+    # P(Ap ≥ 1) where Ap ~ Binom(Hp, phi(alpha)) and Hp ~ Geom(phi(1-alpha))
+    Apge1 = 1 - phi(1 - alpha) / asc
 
     return HleA * Apge1
 
 
+perasBlockMinSlotsValues = [30, 60] + list(range(90, 990, 90))
+
 for alpha in [0.05, 0.1, 0.15, 0.2]:
-    for perasBlockMinSlots in [30, 60, 90, 120, 150, 300]:
+    for perasBlockMinSlots in perasBlockMinSlotsValues:
         p = Psuccess(alpha, perasBlockMinSlots)
         print(f"alpha = {alpha}, L = {perasBlockMinSlots}: {p}")
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 6))
 
 alpha = np.linspace(0, 0.25, num=20)
 
-for perasBlockMinSlots in [30, 60, 90, 120, 150, 300]:
+for perasBlockMinSlots in perasBlockMinSlotsValues:
     Ps = np.vectorize(lambda a: Psuccess(a, perasBlockMinSlots))(alpha)
     ax1.plot(alpha, Ps, label=f"perasBlockMinSlots = {perasBlockMinSlots}")
-    expected = 90 / Ps / 3600 / 24 / 7
+    expected = 90 / Ps / 3600 / 24 / 365
     ax2.plot(alpha, expected, label=f"perasBlockMinSlots = {perasBlockMinSlots}")
 
 ax1.legend()
 ax1.grid(True)
 ax1.set_xlabel("adversarial stake")
 ax1.set_ylabel("attack success probability")
+ax1.set_yscale("log")
 
 ax2.legend()
 ax2.grid(True)
 ax2.set_xlabel("adversarial stake")
-ax2.set_ylabel("expected time until successful attack [week]")
+ax2.set_ylabel("expected time until successful attack [year]")
 ax2.set_yscale("log")
 
 fig.tight_layout()
