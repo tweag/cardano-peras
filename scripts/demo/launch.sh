30034ce7e77a1752b2af07aa6a115ecd683b448f
Nicolas BACQUEY <nicolas.bacquey@tweag.io>
2025-09-18 13:29:53 +0200

    Separate config creation from runtime, and export static filesx

--- scripts/demo/launch.sh
+++ scripts/demo/launch.sh
@@ -5,10 +5,19 @@ set -e
 CARDANO_CLI=$(realpath bin/cardano-cli)
 CARDANO_NODE=$(realpath bin/cardano-node)
 CARDANO_TESTNET=$(realpath bin/cardano-testnet)
+STATIC_FILES=$(realpath src/ouroboros-consensus/static)
 
 export CARDANO_CLI
 export CARDANO_NODE
 export ENABLE_PERAS=1
 export PERAS_CERT_CONJURING=1
+export PERAS_CERT_CONJURING_STATIC_FILES="$STATIC_FILES"
 
-$CARDANO_TESTNET cardano
+OUTPUT_DIR=testnet
+
+rm -rf "$OUTPUT_DIR"
+$CARDANO_TESTNET create-env --output "$OUTPUT_DIR"
+
+# Modify the configuration files in $OUTPUT_DIR
+
+$CARDANO_TESTNET cardano --node-env "$OUTPUT_DIR"
