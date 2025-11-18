9dade3e642ec501b636514b786fe151bfdf55f09
Agustin Mista <agustin.mista@moduscreate.com>
2025-11-18 15:58:27 +0100

    Add picocss and under construction banner

---
+++ dashboard/src/components/UnderConstruction.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'UnderConstruction',
+  props: {
+    title: String
+  }
+})
+</script>
+
+<template>
+  <main class="container-fluid" id="under-construction">
+    <img id="logo" alt="Tweag logo" width="50%" src="../assets/tweag_logo.svg">
+    <h1>{{ title }}</h1>
+    <p><i>This website is currently under construction.</i></p>
+  </main>
+</template>
+
+<style scoped>
+#under-construction {
+  text-align: center;
+  margin-top: 100px;
+}
+#logo {
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+</style>
