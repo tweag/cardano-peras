942f6b0d1e767602150b6fae932064bb948aaccf
Agustin Mista <agustin.mista@moduscreate.com>
2025-11-20 13:54:37 +0100

    Minor UI tweaks

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -15,35 +15,57 @@ const globalControls = reactive(props.globals)
 </script>
 
 <template>
-  <header class="container-fluid">
-    <h1>{{ title }}</h1>
-  </header>
-  <main>
-    <GlobalControls :controls="globalControls" />
-    <MetricPanel
-      v-for="(metric, index) in metrics"
-      :key="index"
-      :title="metric.title"
-      :description="metric.description"
-      :labels="metric.labels"
-      :globals="globalControls"
-      :controls="reactive(metric.controls)"
-      :compute="metric.compute"
-    />
-  </main>
-  <footer v-if="footer" class="container-fluid">
-    {{ footer }}
-  </footer>
+  <div class="container-fluid wrapper">
+    <nav>
+      <ul>
+        <li>
+          <strong>{{ title }}</strong>
+        </li>
+      </ul>
+      <ul>
+        <li><a href="#">Link 1</a></li>
+        <li><a href="#">Link 2</a></li>
+      </ul>
+    </nav>
+    <main class="content">
+      <GlobalControls :controls="globalControls" />
+      <MetricPanel
+        v-for="(metric, index) in metrics"
+        :key="index"
+        :title="metric.title"
+        :description="metric.description"
+        :labels="metric.labels"
+        :globals="globalControls"
+        :controls="reactive(metric.controls)"
+        :compute="metric.compute"
+      />
+    </main>
+    <footer v-if="footer" class="container-fluid">
+      {{ footer }}
+    </footer>
+  </div>
 </template>
 
 <style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+nav {
+  margin-bottom: 2em;
+}
 header {
   text-align: center;
   margin-top: 2em;
   margin-bottom: 2em;
 }
+main {
+  flex: 1;
+}
 footer {
   text-align: center;
   margin-top: 2em;
+  padding-bottom: 1em;
 }
 </style>
