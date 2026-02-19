57630edb9344ba85d8c6e44338d09fc7832634ff
Nicolas BACQUEY <nicolas.bacquey@tweag.io>
2026-02-19 17:37:02 +0100

    Move performance specific parameters to their respective metric

--- dashboard/src/metrics/ExtraPower.ts
+++ dashboard/src/metrics/ExtraPower.ts
@@ -9,13 +9,56 @@ export default {
       label: 'Neighborhood size',
     },
     y: {
-      label: 'Extra bandwidth per node, in percentage',
+      label: 'Extra bandwidth per node, in CPU percentage',
     },
   },
   legend: {
     position: 'bottom-right',
   },
-  controls: {},
+  controls: {
+    voteGenerationTime: {
+      type: 'spinner',
+      value: 280,
+      min: 10,
+      max: 1_000_000,
+      step: 1,
+      name: 'Vote generation time (μs)',
+      tooltip: "The time it takes to generate a vote with a node's private key",
+    },
+    voteVerificationTime: {
+      type: 'spinner',
+      value: 1400,
+      min: 10,
+      max: 1_000_000,
+      step: 1,
+      name: 'Vote verification time (μs)',
+      tooltip:
+        "The time it takes to check the authenticity of a vote, with it's issuer public key",
+    },
+    certGenerationTime: {
+      type: 'spinner',
+      value: 63000,
+      min: 10,
+      max: 1_000_000,
+      step: 1,
+      name: 'Certificate generation time (μs)',
+      tooltip:
+        'The time it takes to generate a certificate by aggregating votes',
+    },
+    // Certificate verification time is currently unused, because it only matters
+    // when trying to sync in Genesis mode from a historical database, or when trying
+    // to validate a certificate that has been included in a block.
+    // Those are not behaviors of healthy, caught-up Peras nodes.
+    // certVerificationTime: {
+    //   type: 'spinner',
+    //   value: 115000,
+    //   min: 10,
+    //   max: 1_000_000,
+    //   step: 1,
+    //   name: 'Certificate verification time (μs)',
+    //   tooltip: 'The time it takes to check the authenticity of a certificate',
+    // },
+  },
   compute: (ctrl: Record<string, number>) => {
     const minValue = 1
     const maxValue = 100
@@ -23,7 +66,7 @@ export default {
 
     return [
       {
-        label: 'Extra processing power',
+        label: 'Extra processing power (CPU %)',
         data: discretize(
           (neighborhood) =>
             extraPower(
