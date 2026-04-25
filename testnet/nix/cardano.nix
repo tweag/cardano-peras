cf64509e370e430b65a97c7a41eded867baace57
Adithya Kumar <adithya.kumar@tweag.io>
2026-04-25 22:24:03 +0530

    Use custom source-repository instead of managing the dep locally

--- testnet/nix/cardano.nix
+++ testnet/nix/cardano.nix
@@ -8,14 +8,14 @@ let
   # Patched versions of the necessary dependencies
   deps = {
     # Branch: master
-    ouroboros-consensus = pkgs.fetchFromGitHub {
+    ouroboros-consensus = {
       owner = "IntersectMBO";
       repo = "ouroboros-consensus";
       rev = "c87aa760001e60f0f0d3353f793eb089adb917e7";
       sha256 = "sha256-p52sFxbcmciyM9piV7fnkrfClyy+kW7pzBOIEBmstcM=";
     };
     # Branch: master
-    cardano-node = pkgs.fetchFromGitHub {
+    cardano-node = {
       owner = "IntersectMBO";
       repo = "cardano-node";
       rev = "045bc187a36ef0cbd236db902b85dd8f202fb059";
@@ -23,16 +23,23 @@ let
     };
   };
 
+  makeSourceRepoPackage = inp: ''
+    source-repository-package
+      type: git
+      location: https://github.com/${inp.owner}/${inp.repo}.git
+      tag: ${inp.rev}
+      --sha256: ${inp.sha256}
+  '';
+
   # Patch the cardano-node project and extract the cardano-node executable
   cardanoNodeExe =
     exe:
     let
-      project = (import deps.cardano-node { inherit system; }).project.${system};
+      project = (import (pkgs.fetchFromGitHub deps.cardano-node) { inherit system; }).project.${system};
       patchedProject = project.appendModule {
         # Override default packages with patched versions
         cabalProjectLocal = ''
-          packages:
-            ${deps.ouroboros-consensus}
+          ${makeSourceRepoPackage deps.ouroboros-consensus}
         '';
         # Override default GHC to enable compiling with warnings
         # NOTE: using a module is a workaround for
