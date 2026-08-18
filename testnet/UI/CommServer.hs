3e9c1fb4357f97fe21a80c64c5e92e4516fccc69
Adithya Kumar <adithya.kumar@tweag.io>
2026-08-19 03:25:52 +0530

    Remove vote synthesis from the UI

--- testnet/UI/CommServer.hs
+++ testnet/UI/CommServer.hs
@@ -5,7 +5,6 @@ module UI.CommServer where
 
 import Data.IORef (IORef, atomicModifyIORef')
 import qualified Data.Vector as V
-import qualified Data.Text.Lazy as TL
 import Web.Scotty
 import Misc (env_CARDANO_TESTNET_NUM_NODES)
 
@@ -24,8 +23,7 @@ data Advert = Advert
     } deriving (Show)
 
 data PerNodeInfo = PerNodeInfo
-    { pnVoteCreateRequest :: Maybe (Int, String)
-    , pnLatestAdvert :: Advert
+    { pnLatestAdvert :: Advert
     }
 
 type Database = V.Vector PerNodeInfo
@@ -34,21 +32,10 @@ initialDB :: Database
 initialDB =
     V.replicate
         env_CARDANO_TESTNET_NUM_NODES
-        (PerNodeInfo Nothing (Advert 0 0 0 0 0 (replicate 32 ' ') 0))
+        (PerNodeInfo (Advert 0 0 0 0 0 (replicate 32 ' ') 0))
 
 toVecIndex :: Int -> Int
 toVecIndex i = i - 1
-
-addVoteCreateRequestToAllNodes :: (Int, String) -> Database -> Database
-addVoteCreateRequestToAllNodes vote =
-    V.map (\old -> old { pnVoteCreateRequest = Just vote })
-
-removeVoteCreateRequestFrom :: Int -> Database -> Database
-removeVoteCreateRequestFrom nodeIndex db = do
-    let vecIndex = toVecIndex nodeIndex
-        old      = db V.! vecIndex
-        new      = old { pnVoteCreateRequest = Nothing }
-     in db V.// [(vecIndex, new)]
 
 addAdvertInfo :: Int -> Advert -> Database -> Database
 addAdvertInfo nodeIndex advert db = do
@@ -57,30 +44,14 @@ addAdvertInfo nodeIndex advert db = do
         new      = old { pnLatestAdvert = advert }
      in db V.// [(vecIndex, new)]
 
-numVotesInFlight :: Database -> Int
-numVotesInFlight =
-    V.foldl' (\b a -> maybe 0 (const 1) (pnVoteCreateRequest a) + b) 0
-
-getVoteCreatingDetails :: Int -> Database -> Maybe (Int, String)
-getVoteCreatingDetails nodeIndex db =
-    pnVoteCreateRequest $ db V.! toVecIndex nodeIndex
-
 --------------------------------------------------------------------------------
 -- Server
 --------------------------------------------------------------------------------
 
 runHttpServer :: IORef Database -> IO ()
 runHttpServer ioRef = scotty 9000 $ do
-    get "/vote_creation_details" $ do
-        nodeId <- queryParam "node_id"
-        res <-
-            liftIO $ atomicModifyIORef' ioRef $ \old ->
-                ( removeVoteCreateRequestFrom nodeId old
-                , getVoteCreatingDetails nodeId old
-                )
-        text $ TL.pack $ show res
     get "/advert" $ do
-        nodeId <- queryParam "node_id"
+        nodeId <- read . drop 4 <$> queryParam "node_id"
         numCerts <- read <$> queryParam "num_certs"
         numVotes <- read <$> queryParam "num_votes"
         chainLen <- read <$> queryParam "chain_len"
