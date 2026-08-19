bdc58fef14b6f6c65a88f8a5a6d39de233d5d439
Adithya Kumar <adithya.kumar@tweag.io>
2026-08-19 16:21:35 +0530

    Update the commserver to handle debug logs

--- testnet/UI/CommServer.hs
+++ testnet/UI/CommServer.hs
@@ -3,10 +3,13 @@
 
 module UI.CommServer where
 
+import Control.Concurrent.MVar (MVar, newMVar, withMVar)
 import Data.IORef (IORef, atomicModifyIORef')
 import qualified Data.Vector as V
 import Web.Scotty
-import Misc (env_CARDANO_TESTNET_NUM_NODES)
+import Misc (env_CARDANO_TESTNET_NUM_NODES, env_TESTNET_WORK_DIR)
+import System.FilePath ((</>))
+import System.IO (IOMode (AppendMode), hPutStrLn, withFile)
 
 --------------------------------------------------------------------------------
 -- Database
@@ -44,32 +47,50 @@ addAdvertInfo nodeIndex advert db = do
         new      = old { pnLatestAdvert = advert }
      in db V.// [(vecIndex, new)]
 
+env_COMM_SERVER_LOG_FILE :: FilePath
+env_COMM_SERVER_LOG_FILE = env_TESTNET_WORK_DIR </> "comm-server.log"
+
+serializeLogInfo :: Int -> String -> String
+serializeLogInfo nodeId logMsg = "[" ++ show nodeId ++ "] " ++ logMsg
+
+addLogInfo :: MVar () -> Int -> String -> IO ()
+addLogInfo logLock nodeId logMsg =
+    withMVar logLock $ \() -> do
+        withFile env_COMM_SERVER_LOG_FILE AppendMode $ \handle ->
+            hPutStrLn handle (serializeLogInfo nodeId logMsg)
+
 --------------------------------------------------------------------------------
 -- Server
 --------------------------------------------------------------------------------
 
 runHttpServer :: IORef Database -> IO ()
-runHttpServer ioRef = scotty 9000 $ do
-    get "/advert" $ do
-        nodeId <- read . drop 4 <$> queryParam "node_id"
-        numCerts <- read <$> queryParam "num_certs"
-        numVotes <- read <$> queryParam "num_votes"
-        chainLen <- read <$> queryParam "chain_len"
-        perasBoost <- read <$> queryParam "peras_boost"
-        slotNo <- read <$> queryParam "slot_no"
-        blockHash <- queryParam "block_hash"
-        blockNo <- read <$> queryParam "block_no"
-        let advert =
-                Advert
-                    { pnNumVotes = numVotes
-                    , pnNumCerts = numCerts
-                    , pnChainLen = chainLen
-                    , pnPerasBoost = perasBoost
-                    , pnSlotNo = slotNo
-                    , pnBlockHash = blockHash
-                    , pnBlockNo = blockNo
-                    }
-        liftIO $ atomicModifyIORef' ioRef $ \old ->
-            ( addAdvertInfo nodeId advert old
-            , ()
-            )
+runHttpServer ioRef = do
+    logLock <- newMVar ()
+    scotty 9000 $ do
+        get "/log" $ do
+            nodeId <- read . drop 4 <$> queryParam "node_id"
+            logMsg <- queryParam "message"
+            liftIO $ addLogInfo logLock nodeId logMsg
+        get "/advert" $ do
+            nodeId <- read . drop 4 <$> queryParam "node_id"
+            numCerts <- read <$> queryParam "num_certs"
+            numVotes <- read <$> queryParam "num_votes"
+            chainLen <- read <$> queryParam "chain_len"
+            perasBoost <- read <$> queryParam "peras_boost"
+            slotNo <- read <$> queryParam "slot_no"
+            blockHash <- queryParam "block_hash"
+            blockNo <- read <$> queryParam "block_no"
+            let advert =
+                    Advert
+                        { pnNumVotes = numVotes
+                        , pnNumCerts = numCerts
+                        , pnChainLen = chainLen
+                        , pnPerasBoost = perasBoost
+                        , pnSlotNo = slotNo
+                        , pnBlockHash = blockHash
+                        , pnBlockNo = blockNo
+                        }
+            liftIO $ atomicModifyIORef' ioRef $ \old ->
+                ( addAdvertInfo nodeId advert old
+                , ()
+                )
