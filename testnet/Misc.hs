6c14061ba74670304d6884f42ac94a39ceadcf32
Sandy Maguire <sandy.maguire@tweag.io>
2026-09-16 10:22:23 -0700

    Maybe

--- testnet/Misc.hs
+++ testnet/Misc.hs
@@ -95,8 +95,10 @@ module Misc (
 -- Imports
 -------------------------------------------------------------------------------
 
+import Control.Applicative (asum)
 import Control.Concurrent (threadDelay)
 import Data.Function ((&))
+import Data.Maybe (fromMaybe)
 import Data.Word (Word8)
 import Streamly.Data.Array (Array)
 import Streamly.Data.Fold qualified as Fold
@@ -131,7 +133,7 @@ nonEmptyLines inp =
 
 firstNonEmptyLine :: String -> Stream IO (Array Word8) -> IO String
 firstNonEmptyLine tag =
-    Stream.fold (maybe (error $ "Empty: " <> tag) id <$> Fold.one)
+    Stream.fold (fromMaybe (error $ "Empty: " <> tag) <$> Fold.one)
         . nonEmptyLines
 
 printVar :: String -> String -> IO ()
@@ -145,7 +147,7 @@ ensureBlankWorkDir = do
 waitTill :: String -> IO Bool -> IO ()
 waitTill tag action =
     Stream.repeatM (printStep ("Waiting: " <> tag) >> threadDelay 3000000 >> action)
-        & Stream.takeWhile (not . id)
+        & Stream.takeWhile not
         & Stream.fold Fold.drain
 
 waitTillExists :: String -> IO ()
@@ -338,11 +340,10 @@ runCmd cmd args = runCmd' cmdStr
 -- 3. default from PATH
 resolveExecutable :: String -> (Executables -> Maybe FilePath) -> FilePath -> FilePath
 resolveExecutable envVar fromScenario defaultPath =
-    maybe fromScenarioOrDefault id $ unsafePerformIO (lookupEnv envVar)
-  where
-    fromScenarioOrDefault =
-        maybe defaultPath id $
-            topologyExecutables (scenarioConfigTopology scenarioConfig) >>= fromScenario
+  fromMaybe defaultPath $ asum
+    [ unsafePerformIO $ lookupEnv envVar
+    , topologyExecutables (scenarioConfigTopology scenarioConfig) >>= fromScenario
+    ]
 
 {-# NOINLINE cardanoCli #-}
 cardanoCli :: FilePath
@@ -359,7 +360,7 @@ cardanoTestnet = resolveExecutable "CARDANO_TESTNET" execCardanoTestnet "cardano
 {-# NOINLINE cardanoNodeChairman #-}
 cardanoNodeChairman :: FilePath
 cardanoNodeChairman =
-    maybe "cardano-node-chairman" id $ unsafePerformIO (lookupEnv "CARDANO_NODE_CHAIRMAN")
+    fromMaybe "cardano-node-chairman" $ unsafePerformIO (lookupEnv "CARDANO_NODE_CHAIRMAN")
 
 getProtocolMajorVersion :: IO Int
 getProtocolMajorVersion =
